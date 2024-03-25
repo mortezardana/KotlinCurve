9bfe3d467187af1a28cd147d233159b0c69d0720
MorteZA <mortezamrd75@gmail.com>
2024-03-25 11:36:00 +0000

    exploring different prints

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main() {
     println("Hello world!")
+    prints()
+}
+
+fun prints() {
+    println("This is the println method which will print the value passed to it and then end it with an enter.")
+    print("And for this I used a print function to print a value and I can continue... ")
+    print("in this other print method at the end of the last one.")
 }
