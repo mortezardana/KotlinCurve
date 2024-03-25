f080e291f2676f9fc6d72f2b2ad7b865056d9ab8
MorteZA <mortezamrd75@gmail.com>
2024-03-25 12:00:13 +0000

    Data types exploration

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -30,3 +30,14 @@ fun variables() {
     var nullableVar : String?
     val nullableVal : String?
 }
+
+fun dataTypes() {
+    println("Byte can have a value between -128 to 127")
+    println("Short can have a value between -32768 to 32767")
+    println("Int can have a value between -2147483648 to 2147483647")
+    println("Long can have a value between -9223372036854775807 to 9223372036854775807")
+    println("The difference between Float and double is the precision of the floating points which float have 6 or 7 precision while double has 15")
+    println("Float and Double can also be a scientific number with an 'e' or 'E' to indicate power of 10")
+    println("to convert a numeric data type to another type you must use toByte() or toShort() or toDouble() or any other relative built-in methods")
+
+}
